b8bceba442f4cdc0e495dc32328c79663bddf570
YarinAr55 <yarinad2@gmail.com>
2026-08-12 14:24:19 +0300

    Refactor vite.config.js for cleaner formatting

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-2
 import react from "@vitejs/plugin-react";
-3
- 
-4
+
 export default defineConfig({
-5
-plugins: [react()],
-6
-base: "/office-inventory/",
-7
+  plugins: [react()],
+  base: "/office-inventory/",
 });
