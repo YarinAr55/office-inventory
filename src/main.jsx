78e8da3a2b87858be5e972bef62330efcb7c5521
YarinAr55 <yarinad2@gmail.com>
2026-08-12 14:25:27 +0300

    Remove unnecessary lines from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-2
 import ReactDOM from "react-dom/client";
-3
 import App from "./App";
-4
+
  
-5
 ReactDOM.createRoot(document.getElementById("root")).render(
-6
 <React.StrictMode>
-7
 <App />
-8
 </React.StrictMode>
-9
 );
